8f834137e8d12d4feefd4c39ef8de39682b6dc06
Phil Cooper <philip.cooper@openvest.com>
2023-05-09 10:42:17 -0600

    some recurive specter

--- specter.clj
+++ specter.clj
@@ -4,18 +4,49 @@
   (:require [com.rpl.specter :as S :refer :all]))
 
 
-
-;; https://stackoverflow.com/questions/45764946/how-to-find-indexes-in-deeply-nested-data-structurevectors-and-lists-in-clojur
-;; https://clojurians-log.clojureverse.org/specter/2017-08-20.html
+;; From:
+;;   * [StackOverflow](https://stackoverflow.com/questions/45764946/how-to-find-indexes-in-deeply-nested-data-structurevectors-and-lists-in-clojure)
+;;   * [clojureverse](https://clojurians-log.clojureverse.org/specter/2017-08-20.html)
 (defn find-index-route [v data]
-  (let [p (recursive-path [] p
+  (let [p (recursive-path [] RECUR
                           [(if-path map? ALL INDEXED-VALS)
                            (if-path [LAST (pred= v)]
                              FIRST
-                             [(collect-one FIRST) LAST coll? p]
+                             [(collect-one FIRST) LAST coll? RECUR]
                              )])]
     (let [ret (select-first p data)]
       (if (or (nil? ret) (vector? ret)) ret [ret]))))
 
 (find-index-route :a [{:x 3} {:x 4 :y :a}])  ;; => [1 :y]
+
+
+;; From [StackOverflow question](https://stackoverflow.com/questions/76200172/paths-from-root-to-leaf-of-a-binary-tree-in-clojure)
+(def input {:value 10,
+  :left {:value 8, :left {:value 3}, :right {:value 5}},
+  :right {:value 2, :left {:value 1}}})
+
+
+(def leaf-paths
+  (recursive-path [] RECUR
+      (if-path #(some % [:left :right])
+        [(collect-one :value) (submap [:left :right]) MAP-VALS RECUR]
+        :value)))
+
+(select leaf-paths input)
+; => [[10 8 3] [10 8 5] [10 2 1]]
+
+(def leaf-values
+  (recursive-path [] RECUR
+      (if-path #(some % [:left :right])
+        [(submap [:left :right]) MAP-VALS RECUR]
+        :value)))
+
+(select leaf-values input)
+;; => [3 5 1]
+
+(transform leaf-values #(* 100 %) input)
+;; => {:value 10,
+;;     :left {:value 8, :left {:value 300}, :right {:value 500}},
+;;     :right {:value 2, :left {:value 100}}}
+
 
